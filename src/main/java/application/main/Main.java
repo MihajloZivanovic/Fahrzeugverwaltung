0157e6765ea52284ea2c887f3b1e4f1ac1376456
MihajloZivanovic <mihajlo.zivanovic@hotmail.com>
2020-11-18 19:54:20 +0100

    added files and some code

--- src/main/java/application/main/Main.java
+++ src/main/java/application/main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("primaryView.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/application/main/registerVehicle.fxml"));
+        primaryStage.setTitle("grüzi wohl");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
